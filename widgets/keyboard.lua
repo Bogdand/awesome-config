9739aeb6025c092eb318ff9aeb9efc21db7e1704
Bogdan Danilyuk <bogdand@default-value.com>
2012-07-08 11:27:41 +0300

    Added gnome-settings-daemon to autoload. Added current layout indicator dbus listener

--- widgets/keyboard.lua
+++ widgets/keyboard.lua
@@ -6,8 +6,8 @@ keyboardWidget.updateKeyboardLayout = function()
     local key_layout = fd:read()
     fd:close()
     local flag
-    if     key_layout == "00001000" then flag = "ru"
-    elseif key_layout == "00000000" then flag = "en"
+    if     key_layout == "00001002" then flag = "ru"
+    elseif key_layout == "00000002" then flag = "en"
     else 
     	flag = "en"
     end
@@ -17,13 +17,5 @@ keyboardWidget.updateKeyboardLayout = function()
     return
 end
 
-awful.hooks.timer.register(1,  keyboardWidget.updateKeyboardLayout)
--- dbus.add_match("session", "member='LayoutUpdated'")
-
--- dbus.add_match("session", "member='LayoutUpdated'")
--- dbus.add_signal("org.ayatana.dbusmenu", mykey_update)
--- dbus.add_match("session", "member='DeviceChanged'")
--- dbus.add_signal("org.freedesktop.UPower", keyboardWidget.updateKeyboardLayout)
--- dbus.add_match("session", "member=''")
--- dbus.add_signal("org.ayatana.dbusmenu", keyboardWidget.updateKeyboardLayout)
---- keyboard indicator }}}
+dbus.add_match("session", "member='XAyatanaNewLabel'")
+dbus.add_signal("org.kde.StatusNotifierItem", keyboardWidget.updateKeyboardLayout)
