dc37eca75230aea78e0710dad7f572d64be02a04
Bogdan Danilyuk <bogdan@transferwise.com>
2013-06-19 10:47:36 +0300

    Added walpaper image rotation

--- autorun.lua
+++ autorun.lua
@@ -6,4 +6,30 @@ run_once("xscreensaver -no-splash")
 run_once("nm-applet")
 run_once("gnome-settings-daemon")
 
-run_once("sh /home/bogdan/Pictures/nasaBackground.sh")
+
+wallpaperFolder = "/home/bogdand/Pictures/wallpapers/2013_06"
+if file_exists(wallpaperFolder) then
+  x = 0
+
+  -- setup the timer
+  mytimer = timer { timeout = x }
+  mytimer:add_signal("timeout", function()
+
+    -- tell awsetbg to randomly choose a wallpaper from your wallpaper directory
+    os.execute("awsetbg -T -r " .. wallpaperFolder .. "&")
+
+    -- stop the timer (we don't need multiple instances running at the same time)
+    mytimer:stop()
+
+    -- define the interval in which the next wallpaper change should occur in seconds
+    -- (in this case anytime between 10 and 20 minutes)
+    x = math.random( 600, 1200)
+
+    --restart the timer
+    mytimer.timeout = x
+    mytimer:start()
+  end)
+
+  -- initial start when rc.lua is first run
+  mytimer:start()
+end
