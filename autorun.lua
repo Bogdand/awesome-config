89726a70a4f29c853f9e6463db09260dfb1c9943
bogdan <bogdan@transferwise.com>
2014-02-09 00:41:10 +0200

    Clean autorun

--- autorun.lua
+++ autorun.lua
@@ -1,9 +1,14 @@
-run_once("clipit")
 run_once("dropbox start")
-run_once("wmname LG3D")
-run_once("gnome-do")
-run_once("xscreensaver -no-splash")
-run_once("nm-applet")
-run_once("gnome-settings-daemon")
+-- run_once("synapse")
+-- run_once("xrdb ~/.Xresources")
 
-run_once("sh /home/bogdan/Pictures/nasaBackground.sh")
+-- run_once("xinput set-prop 13 \"Device Enabled\" 0")
+-- run_once("setxkbmap -layout \"us,ru\" -option \"grp:alt_shift_toggle\"")
+
+-- run_once("kbdd")
+--run_once("wmname LG3D")
+--run_once("xscreensaver -no-splash")
+--run_once("nm-applet")
+--run_once("gnome-settings-daemon")
+
+--run_once("sh /home/bogdan/Pictures/nasaBackground.sh")
